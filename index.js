63e717e18b94087ec792dfaae44372093ee7b7f9
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-20 00:20:27 +0600

    dashboard table added with users appintment schedules

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     const bookingsCollection = client
       .db('doctorsPortal')
       .collection('bookings');
+    const usersCollection = client.db('doctorsPortal').collection('users');
     //!Use Aggregate to query multiple collection and then merge data
     app.get('/appointmentOptions', async (req, res) => {
       const date = req.query.date;
@@ -112,6 +113,13 @@ async function run() {
      *
      * */
 
+    app.get('/bookings', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
       const query = {
@@ -126,6 +134,12 @@ async function run() {
         return res.send({ acknowledged: false, message });
       }
       const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     });
   } finally {
